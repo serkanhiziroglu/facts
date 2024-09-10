6c13c8d543b66032a6da0d0954f4bdc1d9fe367a
Serkan Hiziroglu <71054911+serkanhiziroglu@users.noreply.github.com>
2024-09-10 14:58:59 -0700

    Initial Commit

---
+++ iFacts/iFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFactsApp.swift
+//  iFacts
+//
+//  Created by Serkan MP on 10.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct iFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
